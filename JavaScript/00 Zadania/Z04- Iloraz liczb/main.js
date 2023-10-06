b49090b0fb142db7c8c44390441a926502bbb3b0
idk6677 <biczkowskakaja@gmail.com>
2023-10-06 09:59:29 +0200

    .

---
+++ JavaScript/00 Zadania/Z04- Iloraz liczb/main.js
@@ -0,0 +1,8 @@
+const a = document.querySelector(`#dzielna`);
+const b = document.querySelector(`#dzielnik`);
+const wynik = document.querySelector(`#wynik`);
+const btn = document.querySelector(`button`);
+
+btn.addEventListener('click', function() {
+    alert('Ktoś mnie kliknął!!!');
+})
