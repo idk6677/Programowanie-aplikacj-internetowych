b1882aa11203ed3a759956e6791e076ca132c2cb
idk6677 <biczkowskakaja@gmail.com>
2023-10-09 13:23:13 +0200

    .

--- JavaScript/00 Zadania/Z04- Iloraz liczb/main.js
+++ JavaScript/00 Zadania/Z04- Iloraz liczb/main.js
@@ -4,5 +4,15 @@ const wynik = document.querySelector(`#wynik`);
 const btn = document.querySelector(`button`);
 
 btn.addEventListener('click', function() {
-    alert('Ktoś mnie kliknął!!!');
+    let liczba_a = parseFloat(a.value);
+    let liczba_b = parseFloat(b.value);
+
+    console.log(liczba_a);
+    console.log(liczba_b);
+
+    let iloraz = liczba_a/liczba_b;
+    // wynik.innerHTML = 'Iloraz liczb wynosi: '+iloraz;
+    wynik.innerHTML = `Iloraz liczb dla dzielnej równej ${liczba_a} i dzielnika równego ${liczba_b} wynosi ${iloraz.toFixed(2)}`
+
+
 })
