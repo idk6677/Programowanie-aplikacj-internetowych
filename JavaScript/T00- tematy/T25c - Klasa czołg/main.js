c51f56ee4726a1aa3fd0e999687daf216015082a
idk6677 <biczkowskakaja@gmail.com>
2024-02-19 13:24:32 +0100

    .

--- JavaScript/T00- tematy/T25c - Klasa czołg/main.js
+++ JavaScript/T00- tematy/T25c - Klasa czołg/main.js
@@ -1,5 +1,30 @@
-class Czołg{
-    nazwa = "";
+class Czolg{
+    nazwa = "Leopard";
     kolor = "szary";
-    amunicja = "7";
+    amunicja = 4;
+
+    info = function(){
+        wynik.innerHTML = `Czołg nazywa się ${this.nazwa}. Jego kolor to ${this.kolor}. Ma ${this.amunicja} sztuk amunicji <br>`;
+    }
+    wystrzal = function (){
+        if (this.amunicja > 0){
+            wynik.innerHTML `Strzał! <br>`;
+            this.amunicja -= 1;
+        }
+        else {
+            wynik.innerHTML = `Brak amunicji <br>`;
+        }
+    }
+    doladuj = function (){
+        this.amunicja += 1;
+        wynik.innerHTML = `Doładowano 1 pocisk <br>`;
+    }
+
 }
+
+let czolg = new Czolg();
+
+czolg.info();
+czolg.wystrzal();
+czolg.wystrzal();
+czolg.doladuj();
