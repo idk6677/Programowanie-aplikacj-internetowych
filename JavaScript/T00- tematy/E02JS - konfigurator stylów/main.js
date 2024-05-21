5cf51bebe4c41fb3ad14db6b27c3aad1695f47f9
idk6677 <biczkowskakaja@gmail.com>
2024-05-21 20:59:45 +0200

    .

---
+++ JavaScript/T00- tematy/E02JS - konfigurator stylów/main.js
@@ -0,0 +1,9 @@
+const btn_i = document.querySelector('#indigo')
+const btn_s = document.querySelector('#steelblue')
+const btn_o = document.querySelector('#olive')
+
+btn_i.addEventListener('click', function (){
+
+
+
+})
