7a6d268c1ce4739fe2f689865b534eaacaf99991
idk6677 <biczkowskakaja@gmail.com>
2024-05-28 14:17:56 +0200

    .

--- JavaScript/T00- tematy/E02JS - konfigurator stylów/main.js
+++ JavaScript/T00- tematy/E02JS - konfigurator stylów/main.js
@@ -2,11 +2,27 @@ const btn_i = document.querySelector('#indigo')
 const btn_s = document.querySelector('#steelblue')
 const btn_o = document.querySelector('#olive')
 let p_b = document.querySelector('#prawy_blok')
+let kolor = document.querySelector('#kolor')
+let rozmiar = document.querySelector('#rozmiar')
 
 btn_i.addEventListener('click', function (){
 
-    let new_bg = document.createElement('div')
-    new_bg.style.backgroundColor = 'indigo';
-    p_b.appendChild(new_bg);
+    p_b.style.backgroundColor = "indigo"
 
 })
+btn_s.addEventListener('click', function (){
+
+    p_b.style.backgroundColor = "steelblue"
+
+})
+btn_o.addEventListener('click', function (){
+
+    p_b.style.backgroundColor = "olive"
+
+})
+
+let k = kolor.value;
+p_b.style.color = k;
+
+let r = rozmiar.value;
+p_b.style.fontStyle = r;
